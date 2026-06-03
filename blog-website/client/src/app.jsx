93ad74da8ee430f12fc0ca34007c2fe3d266f67b
rimuru-OP <106261456+rimuru-OP@users.noreply.github.com>
2026-06-03 16:28:13 +0530

    Configure React frontend structure and Parcel setup

--- blog-website/client/src/app.jsx
+++ blog-website/client/src/app.jsx
@@ -1,7 +1,9 @@
 export default function App(){
     return (
-        <div>
-            <h1>Blog Website</h1>
-        </div>
+        <>
+            <Navbar />
+            <HomePage />
+            <Footer />
+        </>
     );
 }
