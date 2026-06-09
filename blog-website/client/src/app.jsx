64ff0d121a3245bfff9b01b4078c418226c72f49
rimuru-OP <106261456+rimuru-OP@users.noreply.github.com>
2026-06-09 18:59:32 +0530

    React Router setup

--- blog-website/client/src/app.jsx
+++ blog-website/client/src/app.jsx
@@ -1,9 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import Post from "./pages/Post.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+
 export default function App(){
-    return (
-        <>
-            <Navbar />
-            <HomePage />
-            <Footer />
-        </>
-    );
+ return (
+    <BrowserRouter>
+        <Routes>
+            <Route 
+                path="/" 
+                element={<HomePage />} 
+            />
+            <Route 
+                path="/about" 
+                element={<AboutPage />}
+            />
+            <Route 
+                path="/post" 
+                element={<Post />} 
+            />
+            <Route
+                path="/login"
+                element={<LoginPage />}
+            />
+             <Route
+                path="/register"
+                element={<RegisterPage />}
+            />
+
+        </Routes>
+    </BrowserRouter>
+ );
 }
