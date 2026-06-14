f34c43a95a19c874beb4adc17cd87d4a988ef318
rimuru-OP <106261456+rimuru-OP@users.noreply.github.com>
2026-06-14 17:57:00 +0530

    Auth middleware was done to safeguard routes for post related work later

--- blog-website/server/middleware/authMiddleware.js
+++ blog-website/server/middleware/authMiddleware.js
@@ -1,3 +1,34 @@
 const jwt = require("jsonwebtoken");
 
-const protect = async
+const protect = async (req, res, next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+
+        try{
+            //get token
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(
+                token,
+                process.env.JWT_SECRET,
+            )
+
+            //save data
+            req.user = decoded;
+            next(); //continue
+        } catch(err){
+            return res.status(401).json({
+                message: "Not authorized",
+            });
+        }
+    }
+
+    //no token
+    if(!token){
+        return res.status(401).json({
+            message: "No token provided",
+        });
+    }
+}
+
+module.exports = protect;
