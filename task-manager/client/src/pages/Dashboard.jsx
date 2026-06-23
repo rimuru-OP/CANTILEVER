9ea75744289d9041bcfdced0b84e2b9e9ebc4c3b
rimuru-OP <106261456+rimuru-OP@users.noreply.github.com>
2026-06-23 06:17:25 +0530

    removed some dead code and fixed login/register by removing next() as that is already handled by mongoose.

--- task-manager/client/src/pages/Dashboard.jsx
+++ task-manager/client/src/pages/Dashboard.jsx
@@ -1,4 +1,8 @@
+import { useAuth } from "../hooks/useAuth";
+
 export default function Dashboard(){
+    const { user } = useAuth();
+    console.log("user:", user);
     return(
         <>
             <h1>Dashboard</h1>
--- task-manager/server/models/User.js
+++ task-manager/server/models/User.js
@@ -28,7 +28,6 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 12);
-    next();
 });
 
 // Compare password helper
